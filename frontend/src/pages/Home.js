651b780e54b3867ab22b0aca5865f04e4c338fe9
Piyum Kaveeshwara <piyum7020@gmail.com>
2024-01-04 21:47:01 +0530

    fetched data from backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+    fetchWorkout();
+  }, []);
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout.id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 };
